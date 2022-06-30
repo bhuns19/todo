6183e5e55d2515e54158d2d9d225d9f2f28a777f
bhuns19 <96504989+bhuns19@users.noreply.github.com>
2022-06-30 14:47:28 -0400

    Update ManageTasks.js

--- src/ManageTasks.js
+++ src/ManageTasks.js
@@ -1,7 +1,35 @@
 export async function getAllTasks() {
-
         const response = await fetch('http://127.0.0.1:5000/todos');
         let json = await response.json();
         return json
-    
+}
+
+export async function postTask(postBody){
+        const response = await fetch('http://127.0.0.1:5000/todos', {
+                method: "POST",
+                body: JSON.stringify(postBody),
+                headers: {
+                        "Content-Type": "application/json",
+                },
+        });
+        let json = await response.json();
+        return json
+}
+
+export async function patchTask(task_id, patchBody){
+        const response = await fetch(`http://127.0.0.1:5000/todos/${task_id}`, {
+                method: "PATCH",
+                body: JSON.stringify(patchBody),
+                headers: {
+                        "Content-Type": "application/json",
+                },
+        });
+        let json = await response.json();
+        return json
+}
+
+export async function delTask(taskId) {
+        const response = await fetch(`http://127.0.0.1:5000/todos/${taskId}`, {
+          method: "DELETE",
+        });
 }
